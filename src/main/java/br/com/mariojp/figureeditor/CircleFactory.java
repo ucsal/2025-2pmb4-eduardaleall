e3e38fa9f1e08c8638e3bbef11d615b0bd4c1c4f
eduardaleall <eduarda.leal@ucsal.edu.br>
2025-09-15 19:30:35 +0000

    refactor: correções nas formas

--- src/main/java/br/com/mariojp/figureeditor/CircleFactory.java
+++ src/main/java/br/com/mariojp/figureeditor/CircleFactory.java
@@ -6,7 +6,7 @@ import java.awt.geom.Ellipse2D;
 
 public class CircleFactory implements ShapeFactory {
     @Override
-    public Shape ceateShape(Point p) {
+    public Shape createShape(Point p) {
         int radius = 50;
         return new Ellipse2D.Double(p.x - radius, p.y - radius, 2 * radius, 2 * radius);
     }
